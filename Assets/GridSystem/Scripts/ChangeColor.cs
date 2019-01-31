2ed5e15638a227142055ced796dc2d19faeb1bc9
Leandro <leandrodalcanto@gmail.com>
2019-01-31 16:21:53 +0100

    Add files via upload

---
+++ Assets/GridSystem/Scripts/ChangeColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Material[] material;
+    Renderer rend;
+
+    // Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+
+        if (gameObject.CompareTag("wall"))
+        {
+            rend.sharedMaterial = material[1];
+        }
+        else if (gameObject.CompareTag("Trap"))
+        {
+            rend.sharedMaterial = material[2];
+        }   
+
+
+
+    }
+	
+	
+}
