082809d7082ad73261be70b4973e4c942fa9a8aa
Leandro Dal Canto <leandrodalcanto@gmail.com>
2019-01-31 17:55:15 +0100

    Starting the movement script (isSelected = true/false)

---
+++ Assets/GridSystem/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public bool isSelected = false;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+     
+	}
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isSelected = true;
+            Debug.Log("Cultist is selected");
+        }
+    }
+
+}
